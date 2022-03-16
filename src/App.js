1009d1846de092766595111cc82cb049aabd268d
Carlos Valenzuela <cv123760@gmail.com>
2022-03-16 14:33:37 -0700

    create react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+function App() {
+  return (
+    <div className="App">
+
+      <h2>CONTACT US</h2>
+
+      <form>
+
+        <input name="name" placeholder="NAME" type="text" /> 
+        <input name="email" placeholder="EMAIL" type="email" /> 
+        <input className="message" name="message" placeholder="MESSAGE" type="text" /> 
+        
+      </form>
+
+
+    </div>
+  );
+}
+
+export default App;
