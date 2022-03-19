0ca0f5f01b3b80ed07e1d912801cb6727c0c35c7
Carlos Valenzuela <cv123760@gmail.com>
2022-03-19 11:41:22 -0700

    create burger menu

--- src/App.js
+++ src/App.js
@@ -1,85 +1,213 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import axios from "axios"
 
 function App() {
 
   const [email, setEmail] = useState({
-    name: "", 
-    email: "", 
+    name: "",
+    email: "",
     message: ""
   })
 
-  const handleChange = event=>{
+  const handleChange = event => {
     const name = event.target.name
     const value = event.target.value
 
-    setEmail(prev=>{
-      return{...prev, [name]:value}
+    setEmail(prev => {
+      return { ...prev, [name]: value }
     })
 
   }
 
-  const handleClick = ()=>{
+  const handleClick = () => {
     axios.post("new-lead", email)
     setEmail({
-      name: "", 
-      email: "", 
+      name: "",
+      email: "",
       message: ""
     })
   }
 
+  const [visibility, setVisible] = useState(false)
 
+  const toggleVisible = ()=>{
+    setVisible(prev=>!prev)
+  }
 
   return (
-    <div className="App">
+    <div>
+      <main>
+        <div className="containerMain">
+          <nav>
+            <div className="links">
+              <a href="#">PROJECTS</a>
+              <a href="#">ABOUT US</a>
+              <a href="#">CONTACT</a>
+            </div>
 
-      <h3>CONTACT US</h3>
+            <div className="socialMedia">
+              <a target="_blank" href="https://www.facebook.com/Godoy-Exterior-Design-104660158839824">
+                <img src="images/logo-ins.svg" alt="Instagram" />
+              </a>
+              <a target="_blank" href="https://www.facebook.com/Godoy-Exterior-Design-104660158839824">
+                <img src="images/logo-fac.svg" alt="Facebook" />
+              </a>
+              <a target="_blank" href="#">
+                <img src="images/logo-wha.svg" alt="WhatsApp" />
+              </a>
+            </div>
 
-      <form>
+            <div 
+              className="burgerMenu"
+              onClick={toggleVisible}
+              >
 
-        <input 
-          name="name" 
-          placeholder="NAME" 
-          type="text" 
-          onChange={(event=>{
-            handleChange(event)
-          })}
-          value={email.name}
-        /> 
+              <img src="images/burger-menu.svg" alt="" />
+              <div className="bmenu" style={{display:visibility?"block":"none"}}>
+                <a className="navLinks" href="#">PROJECTS</a>
+
+                <a className="navLinks" href="#">ABOUT US</a>
+
+                <a className="navLinks" href="#">CONTACT</a>
+
+
+                <a target="_blank" href="https://www.facebook.com/Godoy-Exterior-Design-104660158839824">
+                  <img src="images/logo-ins.svg" alt="Instagram" />
+                </a>
+
+                <a target="_blank" href="https://www.facebook.com/Godoy-Exterior-Design-104660158839824">
+                  <img src="images/logo-fac.svg" alt="Facebook" />
+                </a>
+
+                <a target="_blank" href="#">
+                  <img src="images/logo-wha.svg" alt="WhatsApp" />
+                </a>
+
+              </div>
+            </div>
+          </nav>
+
+          <div className="logo">
+            <img src="images/Logo.png" alt="" />
+          </div>
+
+        </div>
+      </main>
+
+      <div className="intro">
+
+        <h1>Your Home is Your Pride and Joy</h1>
+
+        <div className="paragraph">
+
+          <p>With over 25 years industry experience, Godoy Exterior Design will make your vision a reality. We will empower
+            you to create the exterior for your home you have always desired. Let our experienced team bring new life to it
+            with the finishing touches you envision.
+          </p>
+
+          <p>We are your exterior finish specialists. Contact us today for a free consultation.</p>
         
-        <input 
-          name="email" 
-          placeholder="EMAIL" 
-          type="email" 
-          onChange={(event=>{
-            handleChange(event)
-          })}
-          value={email.email}
-        /> 
-        
-        <input 
-          className="message" 
-          name="message" 
-          placeholder="MESSAGE" 
-          type="text" 
-          onChange={(event=>{
-            handleChange(event)
-          })}
-          value={email.message}
-        /> 
-        
-        <button 
-          type="submit"
-          onClick={event=>{
-            event.preventDefault()
-            if (email.email.includes("@")) {handleClick()}
-            else{alert("Please enter a valid Email address")}
-          }}
+        </div>
+
+        <div className="videoWraper">
+
+          <iframe src="https://www.youtube.com/embed/MgZ1MheFBuI" title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+
+        </div>
+
+      </div>
+
+      <div className="blueSection">
+
+        <h2>WHAT OUR CLIENTS SAY</h2>
+
+        <div className="reviews">
+
+          <div className="reviewContainer">
+
+            <div className="clientReview">
+
+              <p>Very professional and pleasant staff, definitely one of the best stucco contractors of the valley! If you are looking for quality and on time work Godoy EXD is your go to.</p>
+
+            </div>
+
+            <div className="name">
+              <p>EVALYN W.</p>
+            </div>
+
+            <p className="location">CHANDLER, AZ</p>
+
+          </div>
+
+          <div className="reviewContainer">
+            <div className="clientReview">
+              <p>Great job. Easy to work with. Constant communication. Very Professional and showed up right on schedule every day. </p>
+            </div>
+
+            <div className="name">
+              <p>TANNER M.</p>
+            </div>
+            <p className="location">PHOENIX, AZ</p>
+
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+      <div className="App">
+        <h3>CONTACT US</h3>
+
+        <form>
+
+          <input
+            name="name"
+            placeholder="NAME"
+            type="text"
+            onChange={(event => {
+              handleChange(event)
+            })}
+            value={email.name}
+          />
+
+          <input
+            name="email"
+            placeholder="EMAIL"
+            type="email"
+            onChange={(event => {
+              handleChange(event)
+            })}
+            value={email.email}
+          />
+
+          <input
+            className="message"
+            name="message"
+            placeholder="MESSAGE"
+            type="text"
+            onChange={(event => {
+              handleChange(event)
+            })}
+            value={email.message}
+          />
+
+          <button
+            type="submit"
+            onClick={event => {
+              event.preventDefault()
+              if (email.email.includes("@")) { handleClick() }
+              else { alert("Please enter a valid Email address") }
+            }}
           > SEND</button>
 
 
-      </form>
-      
+        </form>
+
+      </div>
     </div>
   );
 }
